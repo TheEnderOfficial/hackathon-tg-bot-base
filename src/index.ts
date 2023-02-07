fe72369d1a8f298ea534c7ce331f96b1ff03a678
TheEnderOfficial <enderofficial.dp@yandex.ru>
2023-02-07 17:08:06 +0300

    Scenes support

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import {Bot} from "./types";
 import handlers from "./handlers";
 import {PrismaClient} from "@prisma/client";
+import scenes from "./scenes";
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 const db = new PrismaClient();
@@ -11,6 +12,7 @@ bot.context.db = db;
 async function main() {
     await db.$connect();
     handlers.forEach(i => bot.use(i))
+    bot.use(scenes.middleware());
 
     console.log('Launching telegraf bot!')
     await bot.launch();
---
+++ src/scenes/index.ts
@@ -0,0 +1,6 @@
+import {Scenes} from "telegraf";
+import {Context} from "../types";
+
+const stage = new Scenes.Stage<Context>([], {});
+
+export default stage;
